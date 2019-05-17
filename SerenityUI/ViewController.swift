cabc7f36e9f29972b15afe37756e300d938b686d
Eldrian <andieldrian@gmail.com>
2019-05-17 21:15:59 +0700

    Initial Commit

---
+++ SerenityUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SerenityUI
+//
+//  Created by Andi Ikhsan Eldrian on 17/05/19.
+//  Copyright © 2019 Andi Ikhsan Eldrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
